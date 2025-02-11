c29b5201ff6d4e0d6a3250169f3d4ce30467b5e2
猫大名ねこ猫 <48146054+W88DodPECuThLOl@users.noreply.github.com>
2025-02-12 05:29:50 +0900

    Initial commit

---
+++ X1Img2PalData/Palette.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace X1Img2PalData
+{
+	internal class Palette
+    {
+        internal static void Convert(string InSourceFilename, string InOutputFilename, int InColorSize = 16)
+        {
+            List<byte> data = new List<byte>();
+
+            // イメージからパレットを抜き出す
+            List<Color> palette;
+            using(var tmp = new Bitmap(InSourceFilename)) {
+                palette = tmp.Palette.Entries.ToList();
+            }
+
+            // パレット情報(GRB444)
+            //Console.WriteLine("// format GRB444");
+            for(int i = 0; i < InColorSize; i++) {
+                //int a = (i != 0) ? 0xF : 0;
+                int c = (palette[i].B >> 4)
+                    | (((palette[i].R >> 4) & 0xF) << 4)
+                    | (((palette[i].G >> 4) & 0xF) << 8);
+                //Console.WriteLine("0x" + a.ToString("X1") + c.ToString("X3") + ",");
+                data.Add((byte)( c       & 0xff)); // 下位
+                data.Add((byte)((c >> 8) & 0xff)); // 上位
+            }
+
+            File.WriteAllBytes(InOutputFilename, data.ToArray());
+        }
+    }
+}
---
+++ X1Img2PalData/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X1Img2PalData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0) {
+                printUsage();
+                Environment.Exit(1);
+            }
+
+            string sourceFilename = args[0];
+            string outputFilename;
+            if(args.Length > 1) {
+                outputFilename = args[1];
+            } else {
+                outputFilename = Path.ChangeExtension(sourceFilename, ".PAL");
+            }
+
+            if(sourceFilename == outputFilename) {
+                Environment.Exit(2);
+            }
+
+            Palette.Convert(sourceFilename, outputFilename);
+        }
+        static private void printUsage()
+        {
+            System.Console.WriteLine("Palette conversion tool for X1turboZ.");
+            System.Console.WriteLine("X1Img2PalData.exe <sourceImageFilename> [<outputFilename>]");
+            System.Console.WriteLine("    <sourceImageFilename> : Image source file. The extension is png, bmp, etc.");
+            System.Console.WriteLine("    <outputFilename>      : Output file.");
+            System.Console.WriteLine("                            If omitted, the extension of the image source");
+            System.Console.WriteLine("                            source file is changed to 'PAL'.");
+        }
+    }
+}
